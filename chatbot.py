007cce09ca8d0a5afa1bcead87b087db5957c650
kaanan <kaanan99@gmail.com>
2024-01-06 17:25:46 -0800

    Made new chatbot using mini-llama

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from utils import create_chatbot_pipeline, generate_chatbot_output
+from transformers import pipeline
+import torch
+from utils import generate_context_text, get_chatbot_response
 
 st.title("Financial Chatbot")
 
@@ -12,6 +14,12 @@ if "messages" not in st.session_state:
     Hello! I am a financial chatbot who uses marketwatch.com to try and answer your financial questions. 
     It may take me some time to generate a response so please be patient!
     """
+    st.session_state.messages.append(
+        {
+            "role": "system",
+            "content": "You are a financial chatbot who is answer the users question as accurately. Try to use details, be conversational, and work the question into the response",
+        }
+    )
 
     st.session_state.messages.append(
         {
@@ -22,14 +30,14 @@ if "messages" not in st.session_state:
 
 #  Initial Chatbot pipeline object
 if "pipeline" not in st.session_state:
-    model_name = "deepset/roberta-base-squad2"
-    st.session_state.pipeline = create_chatbot_pipeline(model_name)
+    st.session_state.pipeline = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0", torch_dtype=torch.bfloat16, device_map="auto")
 
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    if message["role"] != "system":
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
 query = st.chat_input("Enter query here:")
 
@@ -39,26 +47,79 @@ if query:
     # Display Query
     with st.chat_message("user"):
         st.markdown(query)
-    st.session_state.messages.append(
-        {
+
+    try:
+
+        print(f"\nQuerying: {query}")
+
+        model_input = [
+            {
+                "role": "system",
+                "content": "You are a financial chatbot who is answer the users question as accurately. Try be conversational, concise, and work the question into the response. Use proper grammar in your response",
+            },
+        ]
+
+
+        with st.chat_message("assistant"):
+            st.markdown("Generating Response...")
+
+        # Generate context for the model and add it to the message list
+        print("Starting to generate context")
+        context_texts = generate_context_text(query, 1)
+        for text in context_texts:
+
+            model_input.append(
+                {
+                    "role": "system",
+                    "content": text,
+                }
+            )
+        print("Finished generating context")
+        
+        # Add query to the message list
+        user_query = {
             "role": "user",
             "content": query,
         }
-    )
 
-    try:
-        # Generate and display chaty bot resposne
-        chatbot_response = generate_chatbot_output(query, st.session_state.pipeline)
+        st.session_state.messages.append(user_query)
+        model_input.append(user_query)
+
+        # Generate Response
+        print("Starting to Generate Model Response")
+        model_prompt = st.session_state.pipeline.tokenizer.apply_chat_template(
+            model_input, 
+            tokenize=False, 
+            add_generation_prompt=True,  
+            max_length=2000,
+            truncation=True
+        )
+
+        output = st.session_state.pipeline(
+            model_prompt, 
+            max_new_tokens=256, 
+            do_sample=True, 
+            temperature=0.7, 
+            top_k=50, 
+            top_p=0.95
+        )
+        print("Finished Generating Model Response")
+        
+        chatbot_response = get_chatbot_response(output)
+
+        # Display response
         with st.chat_message("assistant"):
             st.markdown(chatbot_response)
 
+        # Add response to message history
         st.session_state.messages.append(
             {
                 "role": "assistant",
                 "content": chatbot_response,
             }
         )
-    
+
+    # If there is an error in the users query
     except:
         error_message = "I'm sorry, I had trouble understanding your query. Could you please try again?"
 
--- utils.py
+++ utils.py
@@ -1,10 +1,8 @@
 import requests
-import torch
-import transformers
+import re
 
 from bs4 import BeautifulSoup
 from typing import List
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 
 
 def extract_link(header_object:BeautifulSoup) -> str:
@@ -65,50 +63,29 @@ def generate_context_text(query: str, query_limit:int=5) -> List[str]:
 
         # Extract Text
         paragraphs = article_parser.find_all("p")
-        text = " ".join([p.get_text().strip() for p in paragraphs])
+        text = " ".join(
+            [
+                re.sub("\s\s+", " ", p.get_text().strip())
+                for p in paragraphs
+            ]
+        )
 
         context_article_texts.append(text)
 
-    context = " ".join(context_article_texts).strip()
+    return context_article_texts
 
-    return context
-
-def create_chatbot_pipeline(model_name:str) -> transformers.pipelines.question_answering.QuestionAnsweringPipeline:
-    """Creates pipeline object to generate chatbot responses
+def get_chatbot_response(output:str)-> str:
+    """Extracts model response from the output generated
 
     Args:
-        model_name (str): Name of the model to use
+        output (str): The model output
 
     Returns:
-        transformers.pipelines.question_answering.QuestionAnsweringPipeline: The pipeline object
+        str: Response from the model
     """
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForQuestionAnswering.from_pretrained(model_name)
-    model = model.to(torch.float32)
-
-    model_pipeline = pipeline('question-answering', model=model, tokenizer=tokenizer)
-    return model_pipeline
-
-def generate_chatbot_output(
-        query:str,
-        model_pipeline:transformers.pipelines.question_answering.QuestionAnsweringPipeline, 
-        context_document_num:int=5
-        ) ->  str:
-    """Generates a response for the chatbot using web-scraped documents for context
-
-    Args:
-        query (str): The query from the user
-        model_pipeline (transformers.pipelines.question_answering.QuestionAnsweringPipeline): Pipeline object used to generate response
-        context_document_num (int, optional): Number of documents to use for context. Defaults to 5.
-
-    Returns:
-        str: Response for the chatbot
-    """
-
-    context = generate_context_text(query, context_document_num)
-    model_input = {
-        'question': query,
-        'context': f"You are a financial chatbot who is answer the users question as accurately. Try to use details, be conversational, and work the question into the response. Use the following context to help answer questions: {context}"
-     }
-    result = model_pipeline(model_input)
-    return result["answer"]
+    response = (
+        output[0]["generated_text"]
+        .split(">")[-1]
+        .strip()
+    )
+    return response
